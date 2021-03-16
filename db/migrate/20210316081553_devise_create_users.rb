7342e7f5cee97c6af886cbe6a18ccd9a3dcf378a
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-03-16 17:36:47 +0900

    Revert "修正"
    This reverts commit 398539dfce178c609138dcd38584053b4f71b985.

--- db/migrate/20210316081553_devise_create_users.rb
+++ db/migrate/20210316081553_devise_create_users.rb
@@ -4,7 +4,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -34,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false    
+      t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
