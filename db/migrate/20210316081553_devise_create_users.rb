398539dfce178c609138dcd38584053b4f71b985
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-03-16 17:36:39 +0900

    修正

--- db/migrate/20210316081553_devise_create_users.rb
+++ db/migrate/20210316081553_devise_create_users.rb
@@ -4,6 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
+      t.string :nickname,           null: false
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
@@ -33,7 +34,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.datetime :locked_at
 
 
-      t.timestamps null: false
+      t.timestamps null: false    
     end
 
     add_index :users, :email,                unique: true
