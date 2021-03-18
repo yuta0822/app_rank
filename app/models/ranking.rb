5639161deb31f26f873c6dc876982c9ca1bd7514
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-03-18 15:54:29 +0900

    ランキングバリデーション設定

--- app/models/ranking.rb
+++ app/models/ranking.rb
@@ -1,4 +1,10 @@
 class Ranking < ApplicationRecord
   has_many :ranking_users
   has_many :users, through: :ranking_users
+
+  validates :title,    presence: true
+  validates :category, presence: true
+  validates :choice_a, presence: true 
+  validates :choice_b, presence: true 
+
 end
