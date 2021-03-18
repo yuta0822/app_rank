56a8b87edabc754f9ceb49e9106bd815cd0625a4
yuta0822 <77149871+yuta0822@users.noreply.github.com>
2021-03-18 15:54:11 +0900

    データ保存

--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,4 +2,18 @@ class RankingsController < ApplicationController
   def new
     @ranking = Ranking.new
   end
+
+  def create
+    @ranking = Ranking.new(ranking_params)
+    if @ranking.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def ranking_params
+    params.require(:ranking).permit(:title, :category, :choice_a, :choice_b, :choice_c, :choice_d, :choice_e)
+  end
 end
